5a919ccb6a45503633dfee59089a40afe4ef5f7a
AleksandrFR <79194849399@yandex.ru>
2022-03-09 12:14:05 +0300

    realization filestask1 update

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,31 @@ public class Main {
     static Function<String, File> directory = x -> new File("C:\\Users\\sasha\\IdeaProjects\\Games\\" + x);
     static StringBuilder sb = new StringBuilder();
 
-    public static void createDir(String dirName) {
-        File dir = directory.apply(dirName);
-        if (dir.mkdir())
-            System.out.println("Каталог " + dirName + " создан " + dir.getAbsolutePath());
-        sb.append("\nКаталог " + dirName + " создан " + dir.getAbsolutePath());
-    }
-
-    public static void createFile(String fileName) {
-        File newFile = directory.apply(fileName);
-        try {
-            if (newFile.createNewFile())
-                System.out.println("Файл " + fileName + " создан " + newFile.getAbsolutePath());
-            sb.append("\nФайл " + fileName + " создан " + newFile.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
         String[] directories = new String[]{"src", "res", "savegames", "temp", "src\\main", "src\\test", "res\\drawables",
                 "res\\vectors", "res\\icons"};
         String[] files = new String[]{"src\\main\\Main.java", "src\\main\\Utils.java", "temp\\temp.txt"};
-        for (String dir : directories) {
-            createDir(dir);
+
+        Function<String, File> newDirFile = x -> directory.apply(x);
+
+        for (String dirName : directories) {
+            File dir = newDirFile.apply(dirName);
+            if (dir.mkdir()) {
+                System.out.println("Каталог " + dir + " создан " + dir.getAbsolutePath());
+                sb.append("\nКаталог " + dir + " создан " + dir.getAbsolutePath());
+            }
         }
-        for (String file : files) {
-            createFile(file);
+
+        for (String fileName : files) {
+            File file = newDirFile.apply(fileName);
+            try {
+                if (file.createNewFile())
+                    System.out.println("Файл " + fileName + " создан " + file.getAbsolutePath());
+                sb.append("\nФайл " + fileName + " создан " + file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         try (FileWriter writer = new FileWriter("C:\\Users\\sasha\\IdeaProjects\\Games\\temp\\temp.txt", false)) {
             writer.write(sb.toString());
